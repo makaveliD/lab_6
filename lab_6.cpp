7f3a4084ab74f51406eec5a0014110aa8e5a2dca
Vova Zabalotny <vovazabalotny@gmail.com>
2022-12-30 19:38:17 +0200

    add lab

---
+++ lab_6.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int ack(int n, int m) {
+	if (n == 0)
+	{
+		return m + 1;
+	}
+	else if (m == 0) {
+		return ack(n - 1, 1);
+	}
+	else {
+		return ack(n - 1, ack(n, m - 1));
+	}
+
+}
+
+int main()
+{
+	int n, m;
+	int result;
+	cout << "Enter n,m:" << endl;
+	cin >> n >> m;
+	result = ack(n, m);
+	cout << "Result:" << result;
+  return 0;
+}
+
